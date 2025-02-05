2c13b6933277eb27ea42f2494b2e3ecb5c3b8bdf
clabdamilare <damilare@codedlab.com.ng>
2025-02-05 09:50:43 +0100

    Creating middleware for parent,student,teacher,admin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,22 @@ class AuthController extends Controller
         //dd(Hash::make(123456));
 if(!empty(Auth::check()))
 {
-    return redirect('admin/dashbaord');
+    if(Auth::user()->user_type == 1)
+{
+    return redirect('admin/dashboard');
+}
+else if(Auth::user()->user_type == 2)
+{
+    return redirect('teacher/dashboard');
+}
+else if(Auth::user()->user_type == 3)
+{
+    return redirect('student/dashboard');
+}
+else if(Auth::user()->user_type == 4)
+{
+    return redirect('parent/dashboard');
+}
 }
 
 return view('auth.login');
@@ -26,7 +41,22 @@ return view('auth.login');
 
     if(Auth::attempt(['email' => $request->email, 'password' => $request->password], true))
 {
-    return redirect('admin/dashbaord');
+    if(Auth::user()->user_type == 1)
+{
+    return redirect('admin/dashboard');
+}
+else if(Auth::user()->user_type == 2)
+{
+    return redirect('teacher/dashboard');
+}
+else if(Auth::user()->user_type == 3)
+{
+    return redirect('student/dashboard');
+}
+else if(Auth::user()->user_type == 4)
+{
+    return redirect('parent/dashboard');
+}
 }
 else
 {
